938c4a9bbe445f47620319746efdfc138763ddcf
Geir Veras <geirveras@iMac-de-Adonias.local>
2016-04-06 11:07:39 -0300

    trydjando:

---
+++ trydjango/src/newsletter/views.py
@@ -0,0 +1,60 @@
+from django.conf import settings
+from django.shortcuts import render
+from .forms import ContactForm, SignUpForm
+# Create your views here.
+def home(request):
+    title = 'Welcome'
+    form = SignUpForm(request.POST or None)
+    #if request.user.is_authenticated():
+    #    title = "My Title %s" %(request.user)
+
+    #if request.method == "POST":
+    #    print request.POST
+    context = {
+        "template_title": title,
+        "form": form
+    }
+
+    if form.is_valid():
+        #form.save()
+        instance = form.save(commit=False)
+        full_name = form.cleaned_data.get("full_name")
+        if not full_name:
+            full_name = "New full name"
+        instance.full_name = full_name
+        #if not instance.full_name:
+        #    instance.full_name = "usuario"
+        instance.save()
+        context = {
+            "title": "Thank you"
+        }
+
+    return render(request, "home.html", context)
+
+def contact(request):
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        # for key, value in form.cleaned_data.iteritems():
+        #     print key, value
+            #print form.cleaned_data.get(key)
+        form_email = form.cleaned_data.get("email")
+        form_message = form.cleaned_data.get("message")
+        form_full_name = form.cleaned_data.get("full_name")
+        # print email, message, full_name
+        subject = 'Site contact form'
+        from_email = settings.EMAIL_HOST_USER
+        to_email = [from_email, 'geir_veras@hotmail.com']
+        contact_message = "%$: %$ via %$"%(
+            form_full_name,
+            form_message,
+            form_email)
+
+        send_email(subject,
+            contact_message,
+            from_email,
+            [to_email],
+            fail_silently=True)
+    context = {
+        "form": form,
+    }
+    return render(request, "forms.html", context)
